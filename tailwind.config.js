6174c5dd7e5bcc9a325e5e8eef2a4e28289c03f2
Alec Minchington <alec.minch@gmail.com>
2022-04-09 16:01:25 -0400

    Exclude custom tailwind color by default

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,14 @@ exports = module.exports = {
 	mode: 'jit',
 	content: ['./src/**/*.{html,ts,tsx}'],
 	theme: {
-		extend: {
-			colors: {
-				'your-color-name': {
-					'700': '#ff0000',
-					'800': '#00ff00',
-				}
-			},
-		},
+		// extend: {
+		// 	colors: {
+		// 		'your-color-name': {
+		// 			'700': '#ff0000',
+		// 			'800': '#00ff00',
+		// 		}
+		// 	},
+		// },
 	},
 	plugins: [
 		require('@tailwindcss/typography')
