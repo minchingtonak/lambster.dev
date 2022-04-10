75559e3a2b6ce5338b1d8b3dd8a8925248445b8f
Alec <alec.minch@gmail.com>
2022-04-10 10:25:48 -0400

    preact config fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import checker from 'vite-plugin-checker';
+import alias from '@rollup/plugin-alias';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,4 +14,18 @@ export default defineConfig({
 			},
 		}),
 	],
+	build: {
+		rollupOptions: {
+			plugins: [
+				alias({
+					entries: [
+						{ find: 'react', replacement: 'preact/compat' },
+						{ find: 'react-dom/test-utils', replacement: 'preact/test-utils' },
+						{ find: 'react-dom', replacement: 'preact/compat' },
+						{ find: 'react/jsx-runtime', replacement: 'preact/jsx-runtime' },
+					],
+				}),
+			],
+		},
+	},
 });
