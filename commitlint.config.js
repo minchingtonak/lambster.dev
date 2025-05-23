b64297a29f73adcf1eb32eb81fbaa6c9f8537b03
Alec Minchington <alec.minch@gmail.com>
2025-05-22 17:56:18 -0700

    chore: add formatting, linting, commitlint, precommit hook

---
+++ commitlint.config.js
@@ -0,0 +1,2 @@
+// https://github.com/conventional-changelog/commitlint/blob/master/@commitlint/config-conventional/src/index.ts#L29
+export default { extends: ["@commitlint/config-conventional"] };
---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { ignores: ["*.{json,lock}", "**/*.{json,lock}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    rules: { "no-unused-vars": "warn", "react/react-in-jsx-scope": "off" },
+    settings: {
+      react: {
+        version: "detect", // detect React version automatically
+      },
+    },
+  },
+]);
